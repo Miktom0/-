a8d15be69cfdb89fa9eb82a4f3848189e1520bd6
Miktom0 <140895256+Miktom0@users.noreply.github.com>
2024-04-05 21:57:58 +0800

    Add files via upload

---
+++ index.52061.js
@@ -0,0 +1,23 @@
+System.register(["./application.73b49.js"], function (_export, _context) {
+  "use strict";
+
+  var Application, application;
+  function topLevelImport(url) {
+    return System["import"](url);
+  }
+  return {
+    setters: [function (_application73b49Js) {
+      Application = _application73b49Js.Application;
+    }],
+    execute: function () {
+      application = new Application();
+      topLevelImport('cc').then(function (engine) {
+        return application.init(engine);
+      }).then(function () {
+        return application.start();
+      })["catch"](function (err) {
+        console.error(err);
+      });
+    }
+  };
+});
